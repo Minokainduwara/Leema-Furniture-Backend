2efb5ba538425e4432663addae0f0d211313c318
Ishana Irshaff <ishanairshaff28@gmail.com>
2026-05-03 12:58:30 +0530

    feat(coupon): add Coupon repository

--- src/main/java/com/example/demo/repository/CouponRepository.java
+++ src/main/java/com/example/demo/repository/CouponRepository.java
@@ -1,4 +1,14 @@
 package com.example.demo.repository;
 
-public interface CouponRepository {
+import com.example.demo.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CouponRepository extends JpaRepository<Coupon, Integer> {
+	Optional<Coupon> findByCode(String code);
+
+	Optional<Coupon> findByCodeAndIsActiveTrue(String code);
+
+	boolean existsByCode(String code);
 }
