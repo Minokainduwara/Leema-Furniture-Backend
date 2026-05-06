2caf6503ece93a3e2de3c882ca24bf0d9e3685dc
Sasindu-Randika <uduwalage.sasindu.6666@gmail.com>
2026-05-06 13:06:57 +0530

    update new

---
+++ src/main/java/com/example/demo/dto/response/NotificationDTO.java
@@ -0,0 +1,76 @@
+package com.example.demo.dto.response;
+
+public class NotificationDTO {
+    private Integer id;
+    private String title;
+    private String message;
+    private Boolean read;
+    private String type;
+    private Integer userId;
+    private String customerName;
+    private String customerEmail;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getRead() {
+        return read;
+    }
+
+    public void setRead(Boolean read) {
+        read = read;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+}
