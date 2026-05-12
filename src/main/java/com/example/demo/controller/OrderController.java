8464688c7cc09db6c82f04ad40e73aee7178a673
Dilhara Samaranayake <dilharasamaranayake74@gmail.com>
2026-05-12 20:46:04 +0530

    add order controller

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+
 import com.example.demo.entity.Order;
 import com.example.demo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
